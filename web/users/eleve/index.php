e1b84367c8b19123a909238efc4541f2bd635c7c
Jippy <koundounojp39@gmail.com>
2019-05-05 00:15:43 +0000

    modification sur eleve

--- web/users/eleve/index.php
+++ web/users/eleve/index.php
@@ -1,14 +1,54 @@
-<?php
-  define("NOMPAGE", "Accueil");
-  define("ENTETEPAGE", "Entete");
-  include_once("menu.php");
-?>
+
 
 <?php
+    
+    session_start();
+    include_once("../../commun/requetes.class.php");
+    include_once("../../../server/baseConf.php");
+
+
+    $requete=new Requetes(HOSTNAME, BASENAME, USERNAME, PASSWORD);
+
     if(isset($_REQUEST["road"]) && $_REQUEST["road"]=="accueil"){
-        include_once("vues/accueil.php");
+
+      define("NOMPAGE", "Accueil");
+      define("ENTETEPAGE", "Entete");
+      include_once("menu.php");
+
+      include_once("vues/accueil.php");
     }
+    
+    if (isset($_REQUEST['road']) && $_REQUEST['road']=="custom") {
+      # code...    
+      $_SESSION['pseudo'] = $_GET['pseudo'];
+      header("Location: vues/custom.php");
+    }
+
+    if (isset($_GET['road']) && $_GET['road'] == "update") {
+      # code...
+      $password = $_POST['password'];
+      $password1 = $_POST['password1'];
+
+      if ($password == $password1) {
+        # code...
+        //j'insere dans la bdd
+        
+       $test = $requete->updatePassword($_SESSION['pseudo'],md5($password));
+       echo '<meta http-equiv="Refresh" content="0;url=index.php?road=accueil">';
+       exit();       
+        
+      } else {
+        # code...
+        //je redirrige vers le formulaire de custom password
+        header("Location: vues/custom.php");
+
+      }
+      
+      
+    }
+
   
+
 
 ?>
 
--- web/users/eleve/vues/accueil.php
+++ web/users/eleve/vues/accueil.php
@@ -1,1 +1,72 @@
 
+<div class="row">
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            <a class="" href="../admin/index.php?road=ajouter">
+		<div class="info-box blue-bg">
+			<i class="fa fa-edit fa-fw"></i>
+			<div class="count">Ajout</div>
+			<div class="title">Ajouter des utilisateurs</div>
+		</div><!--/.info-box-->
+            </a>
+	</div><!--/.col-->
+
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            <a class="" href="../admin/index.php?road=lister">
+		<div class="info-box brown-bg">
+			<i class="fa fa-table fa-fw"></i>
+			<div class="count">listage</div>
+			<div class="title">Lister des utilisateurs</div>
+		</div><!--/.info-box-->
+            </a>
+	</div><!--/.col-->
+
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+		<div class="info-box dark-bg">
+			<i class="fa fa-thumbs-o-up"></i>
+			<div class="count">Nombre de classes</div>
+			<div class="title">Classes</div>
+		</div><!--/.info-box-->
+	</div><!--/.col-->
+
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+		<div class="info-box green-bg">
+			<i class="fa fa-cubes"></i>
+			<div class="count">1.426</div>
+			<div class="title">Emplois de temps</div>
+		</div><!--/.info-box-->
+	</div><!--/.col-->
+</div><!--/.row-->
+
+<div class="row">
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+		<div class="info-box blue-bg">
+			<i class="fa fa-cloud-download"></i>
+			<div class="count">6.674</div>
+			<div class="title">Download</div>
+		</div><!--/.info-box-->
+	</div><!--/.col-->
+
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+		<div class="info-box brown-bg">
+			<i class="fa fa-shopping-cart"></i>
+			<div class="count">7.538</div>
+			<div class="title">Purchased</div>
+		</div><!--/.info-box-->
+	</div><!--/.col-->
+
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+		<div class="info-box dark-bg">
+			<i class="fa fa-thumbs-o-up"></i>
+			<div class="count">4.362</div>
+			<div class="title">Order</div>
+		</div><!--/.info-box-->
+	</div><!--/.col-->
+
+	<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+		<div class="info-box green-bg">
+			<i class="fa fa-cubes"></i>
+			<div class="count">1.426</div>
+			<div class="title">Stock</div>
+		</div><!--/.info-box-->
+	</div><!--/.col-->
+</div><!--/.row-->
